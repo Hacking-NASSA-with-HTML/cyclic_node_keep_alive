3884bb69b0e7a23725600b8383e8b926c79d3ed4
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-27 08:29:10 +0400

    feat: run in prod mode

--- middleware/wakeUp.js
+++ middleware/wakeUp.js
@@ -5,13 +5,13 @@ export const wakeUp = (req, res) => {
         try {
             const response = await fetch('https://render-back-end-nikky-pedia.onrender.com/')
             // const body = await response.text();
-            console.log(response.ok)
-            console.log(response.status)
+            // console.log(response.ok)
+            // console.log(response.status)
             // console.log(body)
         } catch (error) {
             console.log(error)
         }
-    }, 3000)
+    }, 300000)
     // res.send("Hello World!, I am a cool server on Cyclic!!!")
     res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('Hello World!, I am a cool server on Cyclic!!!')
